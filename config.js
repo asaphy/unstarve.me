1a32774b5df159786afe5dc0fbb0c8a12a4a3db7
Asaph Yuan <asaphy@bu.edu>
2015-04-17 16:44:40 -0400

    multiple markers

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  'secrets' : {
+    'clientId' : 'ZHSGAUGBIV0WWSYMPWXEOA3EZKLV1Q2S0LIZEZXEXNHUVBN5',
+    'clientSecret' : '5STYIB5J4HOEN0VR45HT2CCZAI2U4CHEG03F3M1Y52T5S1ZF'
+  }
+}
+
+var foursquare = require('node-foursquare')(config);
--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,40 @@ var lat;
 var lng;
 var latlng = "42.349851699999995,-71.1069583";
 
-function test (query, callback){ yelp.search({term: "restaurants" + query, sort: "1", ll: latlng, limit: "1", is_closed: 'false'}, function(error, data) {
+var foursquare = (require('foursquarevenues'))('ZHSGAUGBIV0WWSYMPWXEOA3EZKLV1Q2S0LIZEZXEXNHUVBN5', '5STYIB5J4HOEN0VR45HT2CCZAI2U4CHEG03F3M1Y52T5S1ZF');
+
+var params = {
+    "ll": "40.7,-74"
+};
+
+foursquare.getVenues(params, function(error, venues) {
+    if (!error) {
+    }
+});
+
+function getValues(obj, key) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getValues(obj[i], key));
+        } else if (i == key) {
+            objects.push(obj[i]);
+        }
+    }
+    return objects;
+}
+
+var GOOGLE_PLACES_API_KEY = "AIzaSyBKRP-IfvvckxTosqdOUK0bHTwBDECsUyA";
+var GOOGLE_PLACES_OUTPUT_FORMAT = "json";
+
+var GooglePlaces = require("googleplaces");
+var googlePlaces = new GooglePlaces(GOOGLE_PLACES_API_KEY, GOOGLE_PLACES_OUTPUT_FORMAT);
+var parameters;
+var places_id;
+
+
+function queryYelp (query, callback){ yelp.search({term: "restaurants", sort: "1", ll: latlng, limit: "1", is_closed: 'false'}, function(error, data) {
   console.log(error);
   if(error == null){
     console.log("gettingdata")
@@ -28,7 +61,18 @@ function test (query, callback){ yelp.search({term: "restaurants" + query, sort:
     resultLat = resultLocation.coordinate["latitude"]
     resultLon = resultLocation.coordinate["longitude"]
     console.log(results);
-    console.log(resultAddress);
+
+    parameters = {
+      location:[latlng],
+      types:"restaurant",
+      opennow: "true",
+      rankby: "distance"
+    };
+    googlePlaces.placeSearch(parameters, function (error, response) {
+      if (error) throw error;
+      places_id = getValues(response.results, "place_id");
+      console.log(places_id[0]);
+    });
   }
   else{
    results = "Not Found";
@@ -39,7 +83,7 @@ function test (query, callback){ yelp.search({term: "restaurants" + query, sort:
 function run() {
   /* GET home page. */
   router.get('/', function(req, res) {
-    res.render('index', { title: results, address: resultAddress, lat: resultLat, lon: resultLon});
+    res.render('index', { title: results, address: resultAddress, lat: resultLat, lon: resultLon, placesID: places_id});
   });
 }
 
@@ -59,22 +103,16 @@ router.post('/postLatLng', function (req, res) {
     console.log(latlng);
 });
 
-// router.get('/search', function(req,res,next) {
-//   console.log(req.query);
-//   test("Boston", run2);
-//   //res.render('index', { title: results });
-// });
-
 function getData(req, res, next) {
   console.log(req.query);
   console.log(latlng);
-  test(JSON.stringify(req.query), next());
+  queryYelp(JSON.stringify(req.query), next());
 }
 
 function renderData(req,res){
   //added delay for asynchronity
   setTimeout(function(){
-    res.render('index', { title: results, address: resultAddress, lat: resultLat, lon: resultLon });
+    res.render('index', { title: results, address: resultAddress, lat: resultLat, lon: resultLon, placesID: places_id });
   },1000);
 }
 
